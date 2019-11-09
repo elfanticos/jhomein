6dff38e53fff967d1d05f8f21aa2bfc5529fc14e
jmeza <dart.bekicfox@gmail.com>
2019-11-08 21:31:35 -0500

    Nuevos cambios

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,12 +5,23 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { ArticleLastComponent } from './views/article-last/article-last.component';
 
-
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatCardModule,
+  MatDividerModule
+} from '@angular/material';
 @NgModule({
   declarations: [HomeComponent, ArticleLastComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatDividerModule
   ]
 })
 export class HomeModule { }
