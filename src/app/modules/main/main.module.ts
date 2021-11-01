997aefd601447ac83cae19890aa8637351939ed8
jmeza <jhonatan.meza01@gmail.com>
2021-11-01 23:49:14 +0100

    New branch design5

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './views/main/main.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PresentationComponent } from './components/presentation/presentation.component';
-import { MaterialModule } from '../material/material.module';
+import { MaterialModule } from '../../shared/modules/material/material.module';
 import { AboutComponent } from './components/about/about.component';
 import { ServicesComponent } from './components/services/services.component';
 import { ContactMeComponent } from './components/contact-me/contact-me.component';
@@ -21,6 +21,7 @@ import { ProjectsComponent } from './components/proyects/projects.component';
   imports: [
     CommonModule,
     SharedModule,
+    MaterialModule
   ]
 })
 export class MainModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './components/menu/menu.component';
-import { MaterialModule } from '../modules/material/material.module';
+import { MaterialModule } from './modules/material/material.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { SocialAttachBarComponent } from './components/social-attach-bar/social-attach-bar.component';
 import { ModalContactComponent } from './components/modal-contact/modal-contact.component';
